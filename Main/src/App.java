fcb232812e9a5bb102a19d5d215ce31ecb9b38a2
Mateus Pitura <mateuspitura@gmail.com>
2023-04-01 23:22:55 -0300

    test(project): testando variáveis e arrays

--- Main/src/App.java
+++ Main/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int[] teste = {1, 2, 3, 4};
+        System.out.println(teste[1]);
+        String[] mateus = {"teste"};
+        Math.random();
     }
 }

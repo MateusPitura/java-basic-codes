7220f4297b2836d6b644f6efca09b4b6383a1cd8
Mateus Pitura <mateuspitura@gmail.com>
2023-04-02 23:35:03 -0300

    feat(project): adicionado calculo de idade

--- Aula06/src/App.java
+++ Aula06/src/App.java
@@ -1,30 +1,44 @@
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class App {
+public class App implements ActionListener{
+    JButton button;
+    JTextField output;
+    JTextField input;
+    int anoAtual;
+
     public static void main(String[] args) {
+        App gui = new App();
+        gui.go();
+    }
+
+    public void go(){
         JFrame frame = new JFrame("Aula 06");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
 
-        JButton button = new JButton("Calcular");
+        button = new JButton("Calcular");
+        button.addActionListener(this);
 
         JPanel panelA = new JPanel();
-        JPanel panelB = new JPanel();
         JPanel panelC = new JPanel();
 
         Calendar calendario = Calendar.getInstance();
-        int anoAtual = calendario.get(Calendar.YEAR);
+        anoAtual = calendario.get(Calendar.YEAR);
 
         JLabel txtAnoAtual = new JLabel("Ano atual: " + anoAtual);
 
-        JTextField input = new JTextField();
+        input = new JTextField();
         Dimension preferredSize = new Dimension(50, 25);
         input.setPreferredSize(preferredSize);
         JLabel txtExibicao = new JLabel("Ano nascimento: ");
 
         JLabel txtResultado = new JLabel("Sua idade será: ");
+        output = new JTextField();
+        output.setPreferredSize(preferredSize);
 
         Imagem imagem = new Imagem();
         frame.getContentPane().add(BorderLayout.CENTER, imagem);
@@ -34,6 +48,7 @@ public class App {
         panelA.add(input);
         //panelB.add(imagem);
         panelC.add(txtResultado);
+        panelC.add(output);
 
         frame.add(BorderLayout.NORTH, panelA);
         //frame.add(BorderLayout.CENTER, panelB);
@@ -41,5 +56,12 @@ public class App {
         frame.add(BorderLayout.EAST, button);
 
         frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        int nascimento = Integer.parseInt(input.getText());
+        Integer idade = anoAtual - nascimento;
+        String idadeTexto = Integer.toString(idade);
+        output.setText(idadeTexto);
     }
 }
